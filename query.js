fb585c239cef16b619718b112d25df612a26e602
Rajan Agarwal <64426829+rajanwastaken@users.noreply.github.com>
2022-01-28 23:33:04 +0000

    Initial commit

---
+++ query.js
@@ -0,0 +1,9 @@
+function submit() {
+  document.getElementById('buttons').style.display = 'block';
+  document.getElementById('content').innerHTML = '';
+  var val = document.getElementById('search').value;
+  var newelement = document.createElement('script');
+  newelement.src = `https://www.googleapis.com/customsearch/v1?key=AIzaSyCMFO9fTdnsuxMMLjm6Zhh8O4hLC9Kkfaw&cx=003606982592251140240:5xbiwoxb3m0&q=${val}&callback=hndlr`;
+  newelement.id = 'mainscript';
+  document.body.appendChild(newelement);
+}
