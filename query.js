d91d98c0826d8532725c8998f000990a332679c8
Rajan Agarwal <64426829+rajanwastaken@users.noreply.github.com>
2022-01-31 03:34:25 +0000

    updated workshop content

--- query.js
+++ query.js
@@ -3,7 +3,7 @@ function submit() {
   document.getElementById('content').innerHTML = '';
   var val = document.getElementById('search').value;
   var newelement = document.createElement('script');
-  newelement.src = `https://www.googleapis.com/customsearch/v1?key=AIzaSyCMFO9fTdnsuxMMLjm6Zhh8O4hLC9Kkfaw&cx=003606982592251140240:5xbiwoxb3m0&q=${val}&callback=hndlr`;
+  newelement.src = `https://www.googleapis.com/customsearch/v1?key=API_KEY&cx=003606982592251140240:5xbiwoxb3m0&q=${val}&callback=hndlr`;
   newelement.id = 'mainscript';
   document.body.appendChild(newelement);
 }
